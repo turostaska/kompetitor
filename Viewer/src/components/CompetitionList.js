61deb23aa0958a7a3a598a5f8e28db43c85fe786
csizmawarrior <kulcsar.dana89@gmail.com>
2021-11-27 18:24:55 +0100

    backup before getting dirtier

--- Viewer/src/components/CompetitionList.js
+++ Viewer/src/components/CompetitionList.js
@@ -1,15 +1,40 @@
 import React, {Component} from 'react';
+import SingleCompetition from "./singleElements/SingleCompetition";
 
 class CompetitionList extends  Component {
     constructor() {
         super();
-
+        this.state = {
+            competitions: [],
+        }
     }
 
     render(){
+        let competitionListDisplay = this.state.competitions.map((comp) => {
+            return(
+                <SingleCompetition competition={comp} />
+            );
+        });
         return(
-            <p>CompetitionList</p>
-        )
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <td></td>
+                        <td>Extend</td>
+                        <td>Admin</td>
+                        <td>Participant limit</td>
+                        <td>Start Date</td>
+                        <td>Type</td>
+                        <td>Stages</td>
+                        <td>Competitors</td>
+                        <td>Referees</td>
+                    </tr>
+                </thead>
+                <tbody>
+                { this.state.competitions.length === 0 ? "" : competitionListDisplay}
+                </tbody>
+            </table>
+        );
     }
 }
 
--- Viewer/src/components/singleElements/SingleCompetition.js
+++ Viewer/src/components/singleElements/SingleCompetition.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
 
 class SingleCompetition extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        }
+    }
 }
+
+export default SingleCompetition
